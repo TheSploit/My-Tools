1aaff804eb2465823f8f2dffcb3da815b90e613d
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-07-09 03:55:04 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# ToolsKeren
+# GANS BY ⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ
+# Datanglah Ke Blog saya :https://myamazingapk.blogspot.com
+
+<noscript><a href="https://liberapay.com/TheSploit/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
---
+++ TheSploit.sh
@@ -0,0 +1,277 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 sploit.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            Silahkan Menunggu :v("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            Silahkan Menunggu :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            Silahkan Menunggu :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            Silahkan Menunggu :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            Silahkan Menunggu :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $pur"==================="$cy" ============================="
+echo $i"Ôûê"$pu"  AUTHOR  :  ⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ  "$i"                    Ôûê"
+echo $i"Ôûê"$pu"  WhatsApp:  081316577616"$i"                Ôûê"
+echo $i"Ôûê"$pu"  Blogger :  https://myamazingapk.blogspot.com"$i"     Ôûê"
+echo $i"Ôûê"$pu"  YOUTUBE :  TryOne"$i"                 Ôûê"
+echo $pur"==================="$cy" ============================="
+echo
+echo $cy"MeNu BRUTEPORCE NeWS:"
+echo $ku"["$me"1"$ku"]"$i" Efbeh-Gelap"
+echo $ku"["$me"2"$ku"]"$i" Hack-Cam_Phone"
+echo $ku"["$me"3"$ku"]"$i" The-RAT"
+echo $ku"["$me"4"$ku"]"$i" Malicious"
+echo $ku"["$me"5"$ku"]"$i" Trojan-DDOS"
+echo $ku"["$me"6"$ku"]"$i" Spam-Call"
+echo $ku"["$me"7"$ku"]"$i" Spam-Sms"
+echo $ku"["$me"8"$ku"]"$i" Locate_Track"
+echo $ku"["$me"9"$ku"]"$i" Fb-Creat_Free"
+echo $ku"["$me"10"$ku"]"$i" SPAM WHATSHAPP"
+echo $ku"["$me"11"$ku"]"$i" Insta_Hack(Root-Only)"
+echo $ku"["$me"12"$ku"]"$i" XAttacker"
+echo $ku"["$me"13"$ku"]"$i" SPAM SMS BRUTAL TERBARU"
+echo $i"["$me"14"$i"]"$ku" EXIT"
+echo
+echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
+read -p"Ôò¦ÔöÇ>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Efbeh-Gelap
+cd Efbeh-Gelap
+pip2 install -r requirements.txt
+python2 EfbehGelap.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Hack-Cam_Phone
+cd Hack-Cam_Phone
+bash saycheese.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/The-RAT
+cd The-RAT
+python2 A-Rat.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Sploit-Malicious
+cd Sploit-Malicious
+python2 malicious.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Trojan-DDOS_Attack
+cd Trojan-DDOS_Attack
+python3 Trojan.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Spam-SMS-Call
+cd Spam-SMS-Call
+php fakeCall.php
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Spam-SMS-Call
+cd Spam-SMS-Call
+php spam.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Locate_Track
+cd Locate_Track
+bash lokasi.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Fb-Creat_Free
+cd Fb-Creat_Free
+python2 fb-maker.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/TheSploit/Sploit-Insta_Shell
+cd Sploit-Insta_Shell
+chmod +x install.sh
+sudo ./install.sh
+chmod +x instashell.sh
+service tor start
+sudo ./instashell.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/Moham3dRiahi/XAttacker.git
+cd XAttacker
+chmod +x termux-install.sh
+bash termux-install.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/N74NK/brutalsms.git
+cd brutalsms
+python2 brutalsms.py
+fi
+
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "TUNGGU"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan Digunakan Dengan Bijak"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"WAName   :"$i" ⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ"
+echo $ku"Blogger  :"$i" https://myamazingapk.blogspot.com"
+echo $ku"WhatsApp :"$i" 081316577616"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+echo $pur"SUBSCRIBE MY CHANNEL { TryOne }"
+exit
+fi
