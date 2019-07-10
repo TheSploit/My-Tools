7c522d3aeb36dfd12d67904f0cce05de6717066d
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-07-11 00:28:04 +0700

    Update TheSploit.sh

--- TheSploit.sh
+++ TheSploit.sh
@@ -258,13 +258,13 @@ then
 clear
 figlet -f slant "TUNGGU"|lolcat
 sleep 1
-git clone https://github.com/TheSploit/Sploit-Insta_Shell
-cd Sploit-Insta_Shell
-chmod +x install.sh
-sudo ./install.sh
+git clone https://github.com/thelinuxchoice/instashell
+cd instashell
 chmod +x instashell.sh
 service tor start
 sudo ./instashell.sh
+chmod +x install.sh
+sudo ./install.sh
 fi
 
 if [ $pil = 12 ]
