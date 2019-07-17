d18bdf1982ea448304b3ff77305da4983aa3f2ae
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-07-17 18:54:15 +0700

    Update TheSploit.sh

--- TheSploit.sh
+++ TheSploit.sh
@@ -136,7 +136,40 @@ echo $ku"["$me"33"$ku"]"$i" Lazymux"
 echo $ku"["$me"34"$ku"]"$i" BlackHydra"
 echo $ku"["$me"35"$ku"]"$i" Admin Finder"
 echo $ku"["$me"36"$ku"]"$i" Deface Webdav"
-echo $i"["$me"37"$i"]"$ku" EXIT"
+echo $ku"["$me"37"$ku"]"$i" Deface Webdav"
+echo $ku"["$me"38"$ku"]"$i" Installation SQLScan"
+echo $ku"["$me"39"$ku"]"$i" Installation Hash Buster"
+echo $ku"["$me"40"$ku"]"$i" Installation Termux-A"
+echo $ku"["$me"41"$ku"]"$i" Installation hunner"
+echo $ku"["$me"42"$ku"]"$i" Installation SQLMap"
+echo $ku"["$me"43"$ku"]"$i" Installation BinGoo"
+echo $ku"["$me"44"$ku"]"$i" Installation LazySQLMap"
+echo $ku"["$me"45"$ku"]"$i" Installation Diejobu"
+echo $ku"["$me"46"$ku"]"$i" Installation Error Cyber Tools"
+echo $ku"["$me"47"$ku"]"$i" Installation Tuyul BTC"
+echo $ku"["$me"48"$ku"]"$i" Installation Metasploit Framework"
+echo $ku"["$me"49"$ku"]"$i" Installation viSQL"
+echo $ku"["$me"50"$ku"]"$i" Installation OwScan"
+echo $ku"["$me"51"$ku"]"$i" installation Nmap"
+echo $ku"["$me"52"$ku"]"$i" Installation AuxScan"
+echo $ku"["$me"53"$ku"]"$i" Installation Hasher"
+echo $ku"["$me"54"$ku"]"$i" Installation Phising Game"
+echo $ku"["$me"55"$ku"]"$i" Installation Breacher"
+echo $ku"["$me"56"$ku"]"$i" Installation Social Fish"
+echo $ku"["$me"57"$ku"]"$i" Installation cupp"
+echo $ku"["$me"58"$ku"]"$i" Installation Nuyul BTC vers.02"
+echo $ku"["$me"59"$ku"]"$i" Installation Ko-Dork"
+echo $ku"["$me"60"$ku"]"$i" Installation Check HP"
+echo $ku"["$me"61"$ku"]"$i" Installation TheFat-Rat"
+echo $ku"["$me"62"$ku"]"$i" Installation CMSmap"
+echo $ku"["$me"63"$ku"]"$i" Installation ubuntu"
+echo $ku"["$me"64"$ku"]"$i" Installation Ngrok"
+echo $ku"["$me"65"$ku"]"$i" Installation Lokomedia Eksploiters"
+echo $ku"["$me"66"$ku"]"$i" Installation NetHunter"
+echo $ku"["$me"67"$ku"]"$i" Installation Jomscan"
+echo $ku"["$me"68"$ku"]"$i" Installation Vuln Scanner"
+echo $ku"["$me"69"$ku"]"$i" Linux Fedora"
+echo $i"["$me"70"$i"]"$ku" EXIT"
 echo
 echo $pur"⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ["$pur"Tentukan pilihan kamu Sekarang"$pur"]"
 read -p"⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ " pil
@@ -579,6 +612,473 @@ sh BCA.sh
 fi
 
 if [ $pil = 37 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/kuburan/txtool
+mv txtool $HOME
+cd $HOME/txtool
+python2 install.py
+python2 update.py
+txtool
+fi
+
+if [ $pil = 38 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+cd module
+php sqlscan.php
+fi
+
+if [ $pil = 39 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+
+if [ $pil = 40 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install termux-api
+apt install git
+apt install php
+git clone https://github.com/Cvar1984/Termux-A.git
+mv Termux-A $HOME
+cd $HOME/Termux-A
+php run.php
+fi
+
+if [ $pil = 41 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update
+apt install git
+git clone https://github.com/b3-v3r/Hunner.git
+mv Hunner $HOME
+cd $HOME/Hunner
+chmod +x hunner.py
+python2 hunner.py
+fi
+
+if [ $pil = 42 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $pil = 43 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/Hood3dRob1n/BinGoo
+mv BinGoo $HOME
+cd $HOME/BinGoo
+bash bingoo
+fi
+
+if [ $pil = 44 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/verluchie/termux-lazysqlmap.git
+mv termux-lazysqlmap $HOME
+cd $HOME/termux-lazysqlmap
+chmod 777 install.sh
+./install.sh
+lazysqlmap
+fi
+
+if [ $pil = 45 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt install git
+apt install php
+git clone https://github.com/alintamvanz/diejoubu
+mv diejoubu $HOME
+cd $HOME/diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+if [ $pil = 46 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+git clone https://github.com/MrKeepSmile/errorcybertool.git
+mv errorcybertool $HOME
+cd $HOME/errorcybertool
+chmod 777 errorcyber
+./errorcyber
+fi
+
+if [ $pil = 47 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install php
+pkg install wget
+wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
+mv tuyul.php $HOME
+php $HOME/tuyul.php
+fi
+
+if [ $pil = 48 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+echo "\033[33;1Kunjungi Youtube Saya:" "\033[36;1m https://www.youtube.com/channel/UCyNZMCmIT1lVxC0d1QUwqyA?view_as=subscriber"
+sleep 5
+mv metasploit-framework $HOME
+cd $HOME/metasploit-framework
+./msfconsole
+fi
+
+if [ $pil = 49 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/blackvkng/viSQL
+mv viSQL $HOME
+cd $HOME/viSQL
+python2 -m pip install -r requirements.txt
+python2 viSQL.py
+read -p "[Masukan Website Target]>" target
+python2 viSQL.py -t target
+fi
+
+if [ $pil = 50 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install php
+git clone https://github.com/Gameye98/OWScan.git
+mv OWScan $HOME
+cd $HOME/OWScan
+php owscan.php
+fi
+
+if [ $pil = 51 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install sh
+pip2 install sh
+git clone https://github.com/Gameye98/AstraNmap
+mv AstraNmap $HOME
+cd $HOME/AstraNmap
+sh astranmap.sh
+fi
+
+if [ $pil = 52 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/Gameye98/Auxscan2.0.git
+mv Auxscan2.0 $HOME
+cd $HOME/Auxscan2.0
+python2 auxscan.py
+fi
+
+if [ $pil = 53 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/CiKu370/hasher.git
+mv hasher $HOME
+cd $HOME/hasher
+python2 hash.py 
+fi
+
+if [ $pil = 54 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/senitopeng/PhisingGame.git
+mv PhisingGame $HOME
+cd $HOME/PhisingGame
+python2 phising.py
+fi
+
+if [ $pil = 55 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install python2
+pkg install git
+git clone https://github.com/UltimateHackers/Breacher.git
+mv Breacher $HOME
+cd $HOME/Breacher
+python2 breacher.py
+fi
+
+if [ $pil = 56 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install php
+pkg install python2
+pkg install git
+pkg install curl
+git clone https://github.com/Lexiie/SocialFish.git
+mv SocialFish $HOME
+cd $HOME/SocialFish
+pip2 install wget
+python2 SocialFish.py
+fi
+
+if [ $pil = 57 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/mebus/cupp.git
+mv cupp $HOME
+cd $HOME/cupp
+python2 cupp.py
+fi
+
+if [ $pil = 58 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install python
+pkg install git
+pkg install php
+git clone https://github.com/Cvar1984/pemulungBTC.git
+mv pemulungBTC $HOME
+cd $HOME/pemulungBTC
+php autoload.php
+php mulung.php
+fi
+
+if [ $pil = 59 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg update && pkg upgrade
+pkg install git 
+pkg install python2
+git clone https://github.com/ciku370/ko-dork.git
+mv ko-dork $HOME
+cd $HOME/ko-dork
+python2 dork.py
+fi
+
+if [ $pil = 60 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+pkg install neofetech
+neofetech
+fi
+
+if [ $pil = 61 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+git clone https://github.com/Screetsec/TheFatRat.git
+mv TheFatRat $HOME
+cd $HOME/TheFatRat
+chmod +x setup.sh 
+./setup.sh
+fi
+
+if [ $pil = 62 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt-get update 
+apt-get upgrade
+pkg install python
+apt-get install git
+git clone https://github.com/Dionach/CMSmap.git
+mv CMSmap $HOME
+cd $HOME/CMSmap
+ls
+fi
+
+if [ $pil = 63 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+mv termux-ubuntu $HOME
+cd $HOME/~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+fi
+
+if [ $pil = 64 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+ls
+fi
+
+if [ $pil = 65 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+pkg install php
+pkg install wget
+wget https://pastebin.com/raw/sPpJRjCZ -O lokomedia.php
+mv lokomedia.php $HOME
+cd
+php lokomedia.php 
+fi
+
+if [ $pil = 66 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+mv Nethunter-In-Termux $HOME
+cd $HOME/Nethunter-In-Termux
+chmod 777 kalinethunter
+sh kalinethunter
+fi
+
+if [ $pil = 67 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/rezasp/joomscan.git
+mv joomscan $HOME
+cd $HOME/joomscan
+ls
+fi
+
+if [ $pil = 68 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/kereh/VulnScaner
+mv VulnScaner $HOME
+cd $HOME/VulnScaner
+python2 VulnScaner.py
+fi
+
+if [ $pil = 69 ]
+then
+clear
+figlet -f slant "TUNGGU"|lolcat
+sleep 1
+apt update && apt upgrade
+apt-get install git
+apt install wget
+git clone https://github.com/nmilosev/termux-fedora.git
+mv termux-fedora $HOME
+cd $HOME/termux-fedora
+chmod +x termux-fedora.sh
+sh termux-fedora.sh
+fi
+
+if [ $pil = 70 ]
 then
 clear
 figlet -f slant "TUNGGU"|Ulolcat
