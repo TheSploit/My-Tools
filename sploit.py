7674cfbead795f60fc01dd903ea18ed0f0c45244
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-08-12 23:24:04 +0700

    Update sploit.py

--- sploit.py
+++ sploit.py
@@ -1,6 +1,6 @@
 import os, sys
 
-print ("\033[1;34mSilahkan Masukkan ID & Password Anda")
+print ("\033[1;34mSilahkan Masukkan ID & Password My-Tools")
 
 print ("\033[1;35matau silahkan Hubungi 081316577616 ")
 print ("\033[1;32mJgn Ngasal Cok ntr salah ")
