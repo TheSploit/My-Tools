6faac675d84bf941b3ea876381b5bbe0fb9dafae
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-08-12 21:49:50 +0700

    Update sploit.py

--- sploit.py
+++ sploit.py
@@ -1,9 +1,9 @@
 import os, sys
 
-print ("\033[1;32mSilahkan Masukkan ID & Password Anda")
+print ("\033[1;34mSilahkan Masukkan ID & Password Anda")
 
-print ("\033[1;32matau silahkan Hubungi 081316577616 ")
-
+print ("\033[1;35matau silahkan Hubungi 081316577616 ")
+print ("\033[1;32mJgn Ngasal Cok ntr salah ")
 ID = 'TheSploit'      
 
 password = 'Sploit1109'
